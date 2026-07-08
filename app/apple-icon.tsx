5b2a31f34e7aa690b18942286078b7fe2ee194cf
AGFH5300 <dxb.avg@gmail.com>
2026-07-09 00:33:36 +0530

    Enlarge Apple icon logo render

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -7,8 +7,8 @@ export const contentType = 'image/png'
 export default function AppleIcon() {
   return new ImageResponse(
     (
-      <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#ffffff' }}>
-        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="150" height="150" style={{ width: 150, height: 150, objectFit: 'contain' }} />
+      <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', background: '#ffffff' }}>
+        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="245" height="245" style={{ width: 245, height: 245, objectFit: 'contain' }} />
       </div>
     ),
     size,
