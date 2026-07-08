d85aa5a26092f571e68ba16e2da6168a981284f0
AGFH5300 <dxb.avg@gmail.com>
2026-07-09 00:33:27 +0530

    Enlarge favicon logo render

--- app/icon.tsx
+++ app/icon.tsx
@@ -7,8 +7,8 @@ export const contentType = 'image/png'
 export default function Icon() {
   return new ImageResponse(
     (
-      <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#ffffff' }}>
-        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="32" height="32" style={{ width: 32, height: 32, objectFit: 'contain' }} />
+      <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', background: '#ffffff' }}>
+        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="52" height="52" style={{ width: 52, height: 52, objectFit: 'contain' }} />
       </div>
     ),
     size,
