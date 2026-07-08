5e323066779c674d054ea5b3b59b1a8b2472e608
AGFH5300 <dxb.avg@gmail.com>
2026-07-09 00:42:59 +0530

    Make favicon mark fill icon box

--- app/icon.tsx
+++ app/icon.tsx
@@ -8,7 +8,7 @@ export default function Icon() {
   return new ImageResponse(
     (
       <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', background: '#ffffff' }}>
-        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="52" height="52" style={{ width: 52, height: 52, objectFit: 'contain' }} />
+        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="74" height="74" style={{ width: 74, height: 74, objectFit: 'contain' }} />
       </div>
     ),
     size,
