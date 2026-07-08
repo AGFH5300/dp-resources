1f687372f7309b99592d795cd1342ea913310b69
AGFH5300 <dxb.avg@gmail.com>
2026-07-09 00:15:07 +0530

    Use clean public logo path for favicon route

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,5 +1,4 @@
 import { ImageResponse } from 'next/og'
-import logoMark from './ChatGPT Image Jul 8, 2026, 11_40_32 PM.png'
 
 export const runtime = 'edge'
 export const size = { width: 32, height: 32 }
@@ -9,7 +8,7 @@ export default function Icon() {
   return new ImageResponse(
     (
       <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#ffffff' }}>
-        <img src={logoMark.src} alt="DP Resources" width="32" height="32" style={{ width: 32, height: 32, objectFit: 'contain' }} />
+        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="32" height="32" style={{ width: 32, height: 32, objectFit: 'contain' }} />
       </div>
     ),
     size,
