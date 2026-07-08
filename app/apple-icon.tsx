8f9795be58701312b86a4028e5ce9d9e73b9bd46
AGFH5300 <dxb.avg@gmail.com>
2026-07-09 00:55:40 +0530

    Center Apple icon with equal white padding

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -7,8 +7,8 @@ export const contentType = 'image/png'
 export default function AppleIcon() {
   return new ImageResponse(
     (
-      <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', background: '#ffffff' }}>
-        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="245" height="245" style={{ width: 245, height: 245, objectFit: 'contain' }} />
+      <div style={{ width: 180, height: 180, display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#ffffff' }}>
+        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="130" height="130" style={{ width: 130, height: 130, objectFit: 'contain', display: 'block' }} />
       </div>
     ),
     size,
