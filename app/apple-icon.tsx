052f4e003e2e45e58a65c672dd343e25c2eb41fb
AGFH5300 <dxb.avg@gmail.com>
2026-07-09 00:15:15 +0530

    Use clean public logo path for Apple icon route

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -1,5 +1,4 @@
 import { ImageResponse } from 'next/og'
-import logoMark from './ChatGPT Image Jul 8, 2026, 11_40_32 PM.png'
 
 export const runtime = 'edge'
 export const size = { width: 180, height: 180 }
@@ -9,7 +8,7 @@ export default function AppleIcon() {
   return new ImageResponse(
     (
       <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#ffffff' }}>
-        <img src={logoMark.src} alt="DP Resources" width="150" height="150" style={{ width: 150, height: 150, objectFit: 'contain' }} />
+        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="150" height="150" style={{ width: 150, height: 150, objectFit: 'contain' }} />
       </div>
     ),
     size,
