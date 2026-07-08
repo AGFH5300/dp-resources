6abebe74597784e846233caa9b77e23cb30b39b1
AGFH5300 <dxb.avg@gmail.com>
2026-07-09 00:55:56 +0530

    Center favicon logo with even padding

--- app/icon.tsx
+++ app/icon.tsx
@@ -8,7 +8,7 @@ export default function Icon() {
   return new ImageResponse(
     (
       <div style={{ width: 32, height: 32, display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#ffffff' }}>
-        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="23" height="23" style={{ width: 23, height: 23, objectFit: 'contain', display: 'block' }} />
+        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="26" height="26" style={{ width: 26, height: 26, objectFit: 'contain', display: 'block' }} />
       </div>
     ),
     size,
