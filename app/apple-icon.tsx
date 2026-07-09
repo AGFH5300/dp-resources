646d8f76541a4d11e64ebb3840be93fa50bec527
AGFH5300 <dxb.avg@gmail.com>
2026-07-09 11:03:36 +0530

    Increase Apple icon mark size

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -7,8 +7,8 @@ export const contentType = 'image/png'
 export default function AppleIcon() {
   return new ImageResponse(
     (
-      <div style={{ width: 180, height: 180, display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#ffffff' }}>
-        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="144" height="144" style={{ width: 144, height: 144, objectFit: 'contain', display: 'block' }} />
+      <div style={{ width: 180, height: 180, display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', background: '#ffffff' }}>
+        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="168" height="168" style={{ width: 168, height: 168, objectFit: 'contain', display: 'block' }} />
       </div>
     ),
     size,
