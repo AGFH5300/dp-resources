4bc963c27b4327103f7b841d4837357c978df3fd
AGFH5300 <dxb.avg@gmail.com>
2026-07-09 20:04:14 +0530

    Use resources domain for Open Graph image asset

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -9,7 +9,7 @@ export default function Image() {
   return new ImageResponse(
     (
       <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#ffffff' }}>
-        <img src="https://dp.resources.anshgupta.cc/brand/dp-wordmark.png" alt="DP Resources" width="1000" height="350" style={{ width: 1000, height: 350, objectFit: 'contain' }} />
+        <img src="https://resources.anshgupta.cc/brand/dp-wordmark.png" alt="DP Resources" width="1000" height="350" style={{ width: 1000, height: 350, objectFit: 'contain' }} />
       </div>
     ),
     size,
