c77e2cae0ae36da03a4c3c0c1505c1731b759baf
AGFH5300 <dxb.avg@gmail.com>
2026-07-09 11:03:25 +0530

    Increase favicon mark size without changing navbar

--- app/icon.tsx
+++ app/icon.tsx
@@ -7,8 +7,8 @@ export const contentType = 'image/png'
 export default function Icon() {
   return new ImageResponse(
     (
-      <div style={{ width: 32, height: 32, display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#ffffff' }}>
-        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="26" height="26" style={{ width: 26, height: 26, objectFit: 'contain', display: 'block' }} />
+      <div style={{ width: 32, height: 32, display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden', background: '#ffffff' }}>
+        <img src="https://dp.resources.anshgupta.cc/brand/dp-logo.png" alt="DP Resources" width="44" height="44" style={{ width: 44, height: 44, objectFit: 'contain', display: 'block' }} />
       </div>
     ),
     size,
