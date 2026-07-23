ffe5777d82afd575c8ccfc2664e5bdbb1b4a4de2
AGFH5300 <dxb.avg@gmail.com>
2026-07-23 22:40:10 +0530

    Persist old-course final assessment metadata

---
+++ supabase/migrations/20260723170955_question_bank_old_course_final_assessment_years.sql
@@ -0,0 +1,10 @@
+update public.dp_qb_courses
+set syllabus_label = 'Legacy syllabus · Final assessment 2024'
+where id in (
+  '401de926-a007-52aa-8f31-c7efe2c14707',
+  'a6d5d32e-4a3a-5de2-88c6-04756fa62920',
+  'b5979aba-cbc1-5197-9cee-89aa9bddf690',
+  '0de73762-0570-5f14-a7fe-12c4da0a38d3',
+  'bbe82b4a-6274-53d7-b8c2-f9a5127bfe54',
+  '7af9584c-a3d8-5724-aa53-4545dba9563a'
+);
